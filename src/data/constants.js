1262a7c86ceed9711b6009c060f399cdb1d269e6
vivekjha9955 <vivekjha6814@gmail.com>
2026-05-28 10:43:16 +0530

    new

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,7 +2,7 @@
 import FirstCaraousel from "../assets/FirstCaraousel.png";
 import SecondCarousel from "../assets/SecondCaraousel.png";
 import ThirdCaraousel from "../assets/ThirdCaraousel.png";
-import FourthCaraousel from "../assets/FirstCaraousel.png";
+import FourthCaraousel from "../assets/FourthCaraousel.png";
 export const NAV_LINKS = [
   { label: "Home", href: "#home" },
   { label: "About", href: "#about" },
